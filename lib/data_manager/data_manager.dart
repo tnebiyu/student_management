0b9ccdc097e653e9718a40a73d1fe0508a562fe8
Nebiyu <nebiyu28@gmail.com>
2023-02-18 10:12:57 +0300

    student data management or service created

---
+++ lib/data_manager/data_manager.dart
@@ -0,0 +1,51 @@
+import 'dart:html';
+import '../model/student_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class StudentManagement{
+List<Student>? _student;
+  List<RegisteredStudent> registeredList  = [];
+  fetchStudent() async {
+    try {
+      const url = 'http://localhost:8091/api/v1/students';
+      var response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        _student = [];
+        var decodedData = jsonDecode(response.body) as List<dynamic>;
+        for (var json in decodedData) {
+          _student?.add(Student.fromJson(json));
+        }
+      } else {
+        throw Exception("Error loading data");
+      }
+    } catch (e) {
+      throw Exception("Error loading data");
+    }
+  }
+  Future<List<Student>> getStudent() async{
+    if(_student == null){
+      await fetchStudent();
+    }
+    return _student!;
+
+  }
+
+  void addStudent(Student student) {
+    bool isFound = false;
+    for(var item in registeredList){
+      if(item.student.id == student.id){
+        isFound =true;
+      }
+
+    }
+    if(!isFound){
+      registeredList.add(RegisteredStudent( student: student));
+    }
+  }
+  removeStudent(Student student) {
+    registeredList.removeWhere((element) => element.student.id == student.id);
+  }
+}
+
